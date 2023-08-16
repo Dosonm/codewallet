44fe45a5d6cc32acb4c968d51d83ef2393cc94cc
dosonm <dennny0108@naver.com>
2023-08-16 16:12:49 +0900

    okay

--- js/to do list/tdl.js
+++ js/to do list/tdl.js
@@ -1,13 +1,48 @@
-let ttop = document.getElementById('top');
+let topselector = document.getElementById('toplist');
 
-let ip = function(){
+let change = function(){
     if(window.event.keyCode === 13){   //함수 내에서 event값을 사용하려면 반드시 그 값을 변수로 넘겨야한다
-        let txt = document.getElementById("tt").value;
-        let li = document.createElement('li');
-        let sp = document.createElement('span');
+        let textinputboxvalue = document.getElementById("textinputbox").value;
+        let newli = document.createElement('li');
+        let newspan = document.createElement('span');
+        let newbutton = document.createElement('button');
         
-        sp.textContent = txt;
-        li.appendChild(sp);          //하위속성으로 태그 추가
-        ttop.appendChild(li);
+        newspan.textContent = textinputboxvalue;
+        newli.appendChild(newbutton);
+        newli.appendChild(newspan);          //하위속성으로 태그 추가
+        topselector.appendChild(newli);
+        
+        newli.classList.toggle('list');
+        newspan.classList.add('text');
+        
+        newbutton.addEventListener('click',() => {
+            newspan.classList.toggle('ClearOrNot');
+        })
+        newspan.addEventListener('dblclick',() => {
+            newli.remove();
+        })
+        document.getElementById("textinputbox").value="";
+        saveitems();
     }
 } 
+let alldeletefunction = function(){
+    let allList = document.querySelectorAll('.list');
+    for(let i=0;i<allList.length;i++){
+        allList[i].remove();
+    }
+}
+
+let saveitems = function(){
+    let saved = [];
+    let savedtext = document.getElementsByClassName('text');
+
+    for(let i=0;i<savedtext.length;i++){
+        let localobj = {
+       objtext: savedtext[i],
+       objclass: topselector.children[i].lastChild.classList.contains('ClearOrNot')
+        }
+        saved.push(localobj);
+    }                   //node는 간단히 말해서 html elements 이다 children, childnodes의 차이 구별하기
+    console.log(saved);
+    localStorage.setItem('data',saved);
+}
